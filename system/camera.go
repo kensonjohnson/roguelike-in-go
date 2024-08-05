b0860a11b04edfd383a51c529b033e374d08e1db
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-05 18:24:26 -0500

    Setup update for camera position

---
+++ system/camera.go
@@ -0,0 +1,24 @@
+package system
+
+import (
+	"github.com/kensonjohnson/roguelike-game-go/archetype"
+	"github.com/kensonjohnson/roguelike-game-go/component"
+	"github.com/kensonjohnson/roguelike-game-go/config"
+	"github.com/yohamta/donburi/ecs"
+)
+
+type camera struct{}
+
+var Camera = &camera{}
+
+func (c *camera) Update(ecs *ecs.ECS) {
+	entry := archetype.CameraTag.MustFirst(ecs.World)
+	camera := component.Camera.Get(entry)
+	entry = archetype.PlayerTag.MustFirst(ecs.World)
+	position := component.Position.Get(entry)
+
+	camera.MainCamera.LookAt(
+		float64(((position.X)*config.TileWidth)+config.TileWidth/2),
+		float64((position.Y+2)*config.TileHeight),
+	)
+}
