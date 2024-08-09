227795edd0927386c74eb8ab69ca73b41f7d8acc
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-08 20:08:20 -0500

    Clean up code

--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
-
-	// "github.com/kensonjohnson/roguelike-game-go/archetype"
 	"github.com/kensonjohnson/roguelike-game-go/assets"
 	"github.com/kensonjohnson/roguelike-game-go/config"
 	"github.com/kensonjohnson/roguelike-game-go/system/scene"
-	// "github.com/kensonjohnson/roguelike-game-go/layer"
-	// "github.com/kensonjohnson/roguelike-game-go/system"
 )
 
 type Game struct {
@@ -24,25 +20,15 @@ func (g *Game) configure() {
 		PixelWidth:      config.ScreenWidth * config.TileWidth,
 		PixelHeight:     config.ScreenHeight * config.TileHeight,
 	})
-	// g.ecs = *ecs.NewECS(createWorld())
-	// g.ecs.AddSystem(system.Camera.Update)
-	// g.ecs.AddSystem(system.Turn.Update)
-	// g.ecs.AddSystem(system.UI.Update)
-	// g.ecs.AddRenderer(layer.Background, system.Render.DrawBackground)
-	// g.ecs.AddRenderer(layer.Foreground, system.Render.Draw)
-	// g.ecs.AddRenderer(layer.UI, system.UI.Draw)
-	// g.ecs.AddRenderer(layer.UI, system.DrawMinimap)
-	// g.ecs.AddRenderer(layer.UI, system.Debug.Draw)
+
 }
 
 func (g *Game) Update() error {
-	// g.ecs.Update()
 	g.sceneManager.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// g.ecs.Draw(screen)
 	g.sceneManager.Draw(screen)
 }
 
@@ -50,29 +36,6 @@ func (g *Game) Draw(screen *ebiten.Image) {
 func (g *Game) Layout(w, h int) (int, int) {
 	return config.TileWidth * config.ScreenWidth, config.TileHeight * config.ScreenHeight
 }
-
-// func createWorld() donburi.World {
-// 	world := donburi.NewWorld()
-
-// 	// Create dungeon component
-// 	dungeon := archetype.GenerateDungeon(world)
-
-// 	for index, room := range dungeon.CurrentLevel.Rooms {
-// 		if index == 0 {
-// 			archetype.CreateNewPlayer(world)
-// 		} else {
-// 			archetype.CreateMonster(world, dungeon.CurrentLevel, room)
-// 		}
-// 	}
-
-// 	// Create the UI
-// 	archetype.CreateNewUI(world)
-
-// 	// Create the camera
-// 	archetype.CreateNewCamera(world)
-
-// 	return world
-// }
 
 func main() {
 	assets.MustLoadAssets()
