13b9943881517442115f45b7b3b5c76710e05ace
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-27 17:36:40 -0500

    Add weapons archetype

---
+++ archetype/weapon.go
@@ -0,0 +1,48 @@
+package archetype
+
+import (
+	"github.com/kensonjohnson/roguelike-game-go/component"
+	"github.com/kensonjohnson/roguelike-game-go/items/weapons"
+	"github.com/yohamta/donburi"
+)
+
+var WeaponTag = donburi.NewTag("weapon")
+
+func CreateNewWeapon(world donburi.World, weaponId weapons.WeaponId) {
+	weapon := world.Entry(world.Create(
+		WeaponTag,
+		component.Name,
+		component.Sprite,
+		component.Attack,
+		component.ActionText,
+	))
+
+	weaponData := weapons.Data[weaponId]
+
+	name := component.NameData{
+		Value: weaponData.Name,
+	}
+	component.Name.SetValue(weapon, name)
+
+	sprite := component.SpriteData{
+		Image: weaponData.Sprite,
+	}
+	component.Sprite.SetValue(weapon, sprite)
+
+	attack := component.AttackData{
+		MinimumDamage: weaponData.MinimumDamage,
+		MaximumDamage: weaponData.MaximumDamage,
+		ToHitBonus:    weaponData.ToHitBonus,
+	}
+	component.Attack.SetValue(weapon, attack)
+
+	actionText := component.ActionTextData{
+		Value: weaponData.ActionText,
+	}
+	component.ActionText.SetValue(weapon, actionText)
+
+}
+
+func IsWeapon(entry *donburi.Entry) bool {
+	return entry.HasComponent(WeaponTag)
+}
