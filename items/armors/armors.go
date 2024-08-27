444d3629d99e18b3f36e35b91c79db3bafff85fd
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-27 18:33:31 -0500

    Rework armor data

---
+++ items/armors/armors.go
@@ -0,0 +1,56 @@
+package armors
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/kensonjohnson/roguelike-game-go/assets"
+)
+
+type armorData struct {
+	Name       string
+	Defense    int
+	ArmorClass int
+	Sprite     *ebiten.Image
+}
+
+type ArmorId int
+
+const (
+	LinenShirt ArmorId = iota
+	PaddedArmor
+	Bones
+	PlateArmor
+)
+
+type armorList []armorData
+
+var Data = make(armorList, 0)
+
+func init() {
+	Data[LinenShirt] = armorData{
+		Name:       "Linen Shirt",
+		Defense:    1,
+		ArmorClass: 1,
+		Sprite:     assets.LinenShirt,
+	}
+
+	Data[PaddedArmor] = armorData{
+		Name:       "Padded Armor",
+		Defense:    5,
+		ArmorClass: 6,
+		Sprite:     assets.PaddedArmor,
+	}
+
+	Data[Bones] = armorData{
+		Name:       "Bone",
+		Defense:    3,
+		ArmorClass: 4,
+		Sprite:     assets.Bones,
+	}
+
+	Data[PlateArmor] = armorData{
+		Name:       "Plate Armor",
+		Defense:    15,
+		ArmorClass: 18,
+		Sprite:     assets.PlateArmor,
+	}
+}
