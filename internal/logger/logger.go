a16cb5b9622f298c86bcb06ddb5a51e32897d025
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-28 20:34:56 -0500

    Clean up logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -10,7 +10,7 @@ var (
 	InfoLogger  *log.Logger
 	WarnLogger  *log.Logger
 	ErrorLogger *log.Logger
-	DebugOn     *bool
+	DebugOn     bool
 )
 
 func init() {
@@ -20,42 +20,6 @@ func init() {
 	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ltime|log.Lshortfile)
 }
 
-func SetDebug(flag *bool) {
+func SetDebug(flag bool) {
 	DebugOn = flag
-}
-
-func Debug(v ...any) {
-	if DebugOn != nil && *DebugOn {
-		DebugLogger.Println(v...)
-	}
-}
-
-func Debugf(format string, v ...any) {
-	if DebugOn != nil && *DebugOn {
-		DebugLogger.Printf(format, v...)
-	}
-}
-
-func Info(v ...any) {
-	InfoLogger.Println(v...)
-}
-
-func Infof(format string, v ...any) {
-	InfoLogger.Printf(format, v...)
-}
-
-func Warn(v ...any) {
-	WarnLogger.Println(v...)
-}
-
-func Warnf(format string, v ...any) {
-	WarnLogger.Printf(format, v...)
-}
-
-func Fatal(v ...any) {
-	ErrorLogger.Fatal(v...)
-}
-
-func Fatalf(format string, v ...any) {
-	ErrorLogger.Fatalf(format, v...)
 }
