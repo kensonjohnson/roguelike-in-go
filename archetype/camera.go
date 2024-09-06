350a1c19d7d46ddf1c09e85ab165ea080799e9b7
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-09-05 21:47:22 -0500

    Decouple camera from player position

--- archetype/camera.go
+++ archetype/camera.go
@@ -4,7 +4,6 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/kensonjohnson/roguelike-game-go/component"
 	"github.com/kensonjohnson/roguelike-game-go/internal/config"
-	"github.com/kensonjohnson/roguelike-game-go/internal/logger"
 	"github.com/setanarut/kamera/v2"
 	"github.com/yohamta/donburi"
 )
@@ -12,22 +11,14 @@ import (
 var CameraTag = donburi.NewTag("camera")
 
 func CreateNewCamera(world donburi.World) {
-	var entry *donburi.Entry
-	var ok bool
-	if entry, ok = PlayerTag.First(world); !ok {
-		logger.ErrorLogger.Panic("CreateNewCamera failed: Player not found")
-	}
-	playerPosition := component.Position.Get(entry)
-
-	entry = world.Entry(world.Create(
+	entry := world.Entry(world.Create(
 		CameraTag,
 		component.Camera,
 	))
 
 	cameraData := &component.CameraData{
 		MainCamera: kamera.NewCamera(
-			float64((playerPosition.X*config.TileWidth)+config.TileWidth/2),
-			float64((playerPosition.Y*config.TileHeight)+config.TileHeight/2),
+			0, 0,
 			config.ScreenWidth*config.TileWidth,
 			(config.ScreenHeight-config.UIHeight)*config.TileHeight,
 		),
