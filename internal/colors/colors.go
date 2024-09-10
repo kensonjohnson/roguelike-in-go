5719187c91483bc726121faabdb6ff85c1d82fa2
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-09-09 22:04:27 -0500

    Add colors package

---
+++ internal/colors/colors.go
@@ -0,0 +1,21 @@
+package colors
+
+import "image/color"
+
+var (
+	Blue           = color.RGBA{0, 0, 255, 255}
+	CornflowerBlue = color.RGBA{100, 149, 237, 255}
+	DeepSkyBlue    = color.RGBA{0, 191, 255, 255}
+	Green          = color.RGBA{0, 128, 0, 255}
+	Lime           = color.RGBA{0, 255, 0, 255}
+	Brown          = color.RGBA{165, 42, 42, 255}
+	Peru           = color.RGBA{205, 133, 63, 255}
+	Gray           = color.RGBA{128, 128, 128, 255}
+	LightGray      = color.RGBA{211, 211, 211, 255}
+	DarkGray       = color.RGBA{169, 169, 169, 255}
+	SlateGray      = color.RGBA{112, 128, 144, 255}
+	Silver         = color.RGBA{192, 192, 192, 255}
+	Yellow         = color.RGBA{255, 255, 0, 255}
+	Orange         = color.RGBA{255, 165, 0, 255}
+	Red            = color.RGBA{255, 0, 0, 255}
+)
