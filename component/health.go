2544811683121e9b661a772cc20b57c1a0e61315
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-10-17 15:18:31 -0500

    Add helper functions

--- component/health.go
+++ component/health.go
@@ -8,3 +8,22 @@ type HealthData struct {
 }
 
 var Health = donburi.NewComponentType[HealthData]()
+
+func (h HealthData) Add(amount int) int {
+	current := h.CurrentHealth
+	h.CurrentHealth += amount
+	if h.CurrentHealth > h.MaxHealth {
+		h.CurrentHealth = h.MaxHealth
+	}
+
+	return h.CurrentHealth - current
+}
+
+func (h HealthData) Subtract(amount int) int {
+	current := h.CurrentHealth
+	h.CurrentHealth -= amount
+	if h.CurrentHealth < 0 {
+		h.CurrentHealth = 0
+	}
+	return current - h.CurrentHealth
+}
